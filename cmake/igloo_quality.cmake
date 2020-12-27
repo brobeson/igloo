11b1382a92cf796f68d170242b64b4f6507a6943
brobeson <brobeson@users.noreply.github.com>
2020-12-26 22:52:01 -0700

    I added Cppcheck to the build process

--- cmake/igloo_quality.cmake
+++ cmake/igloo_quality.cmake
@@ -9,3 +9,18 @@ else()
     "I could not find clang-tidy. Clang-tidy checks are disabled."
   )
 endif()
+
+find_program(cppcheck cppcheck)
+if(cppcheck)
+  if(NOT DEFINED CMAKE_CXX_CPPCHECK)
+    set(
+      CMAKE_CXX_CPPCHECK
+      "${cppcheck}"
+      --enable=all
+      --error-exitcode=1
+      --project=${CMAKE_BINARY_DIR}/compile_commands.json
+    )
+  endif()
+else()
+  message(AUTHOR_WARNING "I could not find cppcheck. Cppcheck is disabled.")
+endif()
